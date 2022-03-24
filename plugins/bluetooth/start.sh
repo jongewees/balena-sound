fb0d5e6eda9686cf042a5db076e8f31bfd336c25
jongewees <robgis@gmail.com>
2022-03-24 12:13:06 +0100

    added bluetooth5 drivers and modified configuration of bluetooth plugin

--- plugins/bluetooth/start.sh
+++ plugins/bluetooth/start.sh
@@ -5,4 +5,18 @@ if [[ -n "$SOUND_DISABLE_BLUETOOTH" ]]; then
   exit 0
 fi
 
+#LET OP: geen spatie tussen remount en rw, dus 'remount,rw' ipv 'remount, rw'
+#LET OP: remounten van folders kan alleen als ze al een keer gemount zijn, dus wel /lib/firmware maar niet /lib/firmware/rtl_bt/
+#Als het remounten niet lukt, krijg je tijdens het deployen van de image op de device fouten als 'can´t create rtl8761b_fw.bin' 
+mount -o remount,rw /lib/firmware
+
+#set MOUNTCMD=mount -o remount, rw /
+#echo $($MOUNTCMD> everything.txt 2>&1)
+
+mkdir -p /lib/firmware/rtl_bt/
+
+cp /usr/src/lib/rtl8761b_fw.bin /lib/firmware/rtl_bt/
+
+cp /usr/src/lib/rtl8761b_config.bin /lib/firmware/rtl_bt/
+
 exec /usr/src/bluetooth-agent
